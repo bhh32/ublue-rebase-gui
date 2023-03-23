e9a6e8aa93d8fd4965c89defb2d404d302dde473
Bryan Hyland <bryan.hyland32@gmail.com>
2023-03-23 09:16:19 -0700

    Added version selections

--- ublue_rebase_gui.py
+++ ublue_rebase_gui.py
@@ -1,8 +1,4 @@
 #!/bin/python3
-from multiprocessing import Pool
-import signal
-
-import os
 import sys
 import subprocess
 import gi
@@ -14,13 +10,15 @@ class MainWindow(Gtk.ApplicationWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.set_default_size(600, 250)
+        self.set_default_size(400, 150)
         self.set_title("uBlue Rebase Tool")
 
         self.main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.env_options_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        self.vers_options_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         self.other_options_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         self.button_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        self.status_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
 
         self.bluefin_chk = Gtk.CheckButton(label="Bluefin")
         self.gnome_chk = Gtk.CheckButton(label="Gnome")
@@ -35,12 +33,23 @@ class MainWindow(Gtk.ApplicationWindow):
         self.xfce_chk.set_group(self.bluefin_chk)
         self.kde_chk.set_group(self.bluefin_chk)
 
+        self.thirty_seven_chk = Gtk.CheckButton(label="37")
+        self.thirty_eight_chk = Gtk.CheckButton(label="38")
+        self.latest_chk = Gtk.CheckButton(label="Latest")
+
+        self.thirty_eight_chk.set_group(self.thirty_seven_chk)
+        self.latest_chk.set_group(self.thirty_seven_chk)
+
         self.is_nvidia_chk = Gtk.CheckButton(label="Install Nvidia")
         self.do_reboot_chk = Gtk.CheckButton(label="Auto-reboot")
 
+        self.status_label = Gtk.Label()
+
         self.main_box.append(self.env_options_box)
+        self.main_box.append(self.vers_options_box)
         self.main_box.append(self.other_options_box)
         self.main_box.append(self.button_box)
+        self.main_box.append(self.status_box)
 
         self.env_options_box.append(self.bluefin_chk)
         self.env_options_box.append(self.gnome_chk)
@@ -49,18 +58,25 @@ class MainWindow(Gtk.ApplicationWindow):
         self.env_options_box.append(self.xfce_chk)
         self.env_options_box.append(self.kde_chk)
 
+        self.vers_options_box.append(self.thirty_seven_chk)
+        self.vers_options_box.append(self.thirty_eight_chk)
+        self.vers_options_box.append(self.latest_chk)
+
         self.other_options_box.append(self.is_nvidia_chk)
         self.other_options_box.append(self.do_reboot_chk)
 
         self.rebase_btn = Gtk.Button(label="Rebase")
-        self.rebase_btn.connect('clicked', self.running)
+        self.rebase_btn.connect('clicked', self.run_command)
 
         self.button_box.append(self.rebase_btn)
 
+        self.status_box.append(self.status_label)
+
         self.set_child(self.main_box)
 
-    def run_command(self):
-        running_cmd = subprocess.Popen(["rpm-ostree", "rebase", "--experimental", "ostree-unverified-registry:ghcr.io/ublue-os/" + self.get_env() + ":latest"], stdin=subprocess.PIPE)
+    def run_command(self, button):
+        self.status_label.set_text("Rebasing... Please Wait...")
+        running_cmd = subprocess.Popen(["rpm-ostree", "rebase", "--experimental", "ostree-unverified-registry:ghcr.io/ublue-os/" + self.get_env() + ":latest"], stdin=subprocess.PIPE, stdout=subprocess.DEVNULL)
         running_ret_code = running_cmd.returncode
 
         if running_ret_code == 0:
@@ -72,17 +88,12 @@ class MainWindow(Gtk.ApplicationWindow):
                 if set_kargs_ret_code == 0 and self.do_reboot_chk.get_active():
                     subprocess.Popen(["systemctl", "reboot"])
                 else:
-                    print("Reboot the system for the changes to take effect!")
+                    self.status_label.set_text("Reboot the system for the changes to take effect!")
             
             elif self.do_reboot_chk.get_active():
                 subprocess.Popen(["systemctl", "reboot"])
             else:
-                print("Reboot the system for the changes to take effect!")
-
-    def running(self):
-        run_process = Pool(target=self.run_command, args=())
-        if run_process.exitcode == -signal.SIGTERM:
-            print("The process is complete!")
+                self.status_label.set_text("Reboot the system for the changes to take effect!")
 
     def build_command(self):
         env = self.get_env()
@@ -114,17 +125,19 @@ class MainWindow(Gtk.ApplicationWindow):
         return env
     
     def get_vers(self):
-        vers_cat_process = subprocess.Popen(["cat", "/etc/fedora-release"], stdout=subprocess.PIPE)
-        vers_cut_process = subprocess.Popen(["cut", "-d ", "-f3"], stdin=vers_cat_process.stdout, stdout=subprocess.PIPE)
-        vers_cut_output, vers_cut_errors = vers_cut_process.communicate()
-        vers_cut_process.wait()
+        vers_output = ""
+        if self.thirty_seven_chk.get_active():
+            vers_output = "37"
+        elif self.thirty_eight_chk.get_active():
+            vers_output = "38"
+        else:
+            vers_output = "latest"
 
-        return vers_cut_output
+        return vers_output
 
 
 class RebaseApp(Adw.Application):
     def __init__(self, **kwargs):
-        Gtk.init_check()
         super().__init__(**kwargs)
         self.connect('activate', self.on_activate)
 
@@ -132,6 +145,5 @@ class RebaseApp(Adw.Application):
         self.win = MainWindow(application=app)
         self.win.present()
 
-os.system("export DISPLAY=':0'")
 app = RebaseApp(application_id="com.github.bhh32.ublue_rebase_gui")
 app.run(sys.argv)
